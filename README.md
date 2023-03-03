9470607eba9666e234045a5f0b096d69656bc643
Christopher Haster <geky@geky.net>
2023-03-02 22:15:56 -0600

    Tweaked rbyd benchmarks a bit
    - Updated rbyd benches based on changes: mainly needing separate grow
      tags to adjust the space during mks.
    
    - Removed single-commit benchmark results to make graphs easier to read.
    
    - Rearranged id/attr benchmarks to follow implementation order, attr/id,
      I don't know why but this is what I kept expecting to read.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Rbyd
 
-![rbyd id operations](bench_rbyd.sh.id.svg)
 ![rbyd attr operations](bench_rbyd.sh.attr.svg)
+![rbyd id operations](bench_rbyd.sh.id.svg)
--- bench_rbyd.sh
+++ bench_rbyd.sh
@@ -12,66 +12,36 @@ echo "benching ids $id_count,$id_step attrs $attr_count,$attr_step"
 
 # run benchmarks
 ./scripts/bench.py ./runners/bench_runner -j -Gnor \
-    bench_rbyd_id_commit \
-    bench_rbyd_id_fetch \
-    bench_rbyd_id_lookup \
-    bench_rbyd_id_create \
-    bench_rbyd_id_delete \
-    -DN="range(1,$((id_count+1)),$id_step)" -obench_rbyd.sh.id.csv
-./scripts/bench.py ./runners/bench_runner -j -Gnor \
     bench_rbyd_attr_commit \
     bench_rbyd_attr_fetch \
     bench_rbyd_attr_lookup \
     bench_rbyd_attr_append \
     bench_rbyd_attr_remove \
-    -DN="range(1,$((attr_count+1)),$attr_step)" -obench_rbyd.sh.attr.csv
+    -DCOMMIT=1 \
+    -DN="range(1,$((attr_count+1)),$attr_step)" \
+    -obench_rbyd.sh.attr.csv
+./scripts/bench.py ./runners/bench_runner -j -Gnor \
+    bench_rbyd_id_commit \
+    bench_rbyd_id_fetch \
+    bench_rbyd_id_lookup \
+    bench_rbyd_id_create \
+    bench_rbyd_id_delete \
+    -DCOMMIT=1 \
+    -DN="range(1,$((id_count+1)),$id_step)" \
+    -obench_rbyd.sh.id.csv
 
 # plot results
-./scripts/plotmpl.py bench_rbyd.sh.id.csv -obench_rbyd.sh.id.svg \
-    -l -xN -bORDER -bCOMMIT --dark \
-    -W1600 -H400 --ggplot --y2 --yunits=B \
-    --xlabel="id count" --title="rbyd id operations" \
-    --labels="
-        inorder\,1\,readed,
-        inorder\,1\,proged,
-        inorder\,n\,readed,
-        inorder\,n\,proged,
-        reversed\,1\,readed,
-        reversed\,1\,proged,
-        reversed\,n\,readed,
-        reversed\,n\,proged,
-        random\,1\,readed,
-        random\,1\,proged,
-        random\,n\,readed,
-        random\,n\,proged" \
-    --subplot="-Dcase=bench_rbyd_id_commit -ybench_readed --ylabel=bench_readed --title=rbyd_commit --xticklabels=" \
-        --subplot-below="-Dcase=bench_rbyd_id_commit -ybench_proged --ylabel=bench_proged" \
-    --subplot-right="-Dcase=bench_rbyd_id_fetch -ybench_readed --title=rbyd_fetch -W0.5 --xticklabels= \
-        --subplot-below=\"-Dcase=bench_rbyd_id_fetch -ybench_proged -Y0,1\"" \
-    --subplot-right="-Dcase=bench_rbyd_id_lookup -ybench_readed --title=rbyd_lookup -W0.33 --xticklabels= \
-        --subplot-below=\"-Dcase=bench_rbyd_id_lookup -ybench_proged -Y0,1\"" \
-    --subplot-right="-Dcase=bench_rbyd_id_create -ybench_readed --title=rbyd_create -W0.25 --xticklabels= \
-        --subplot-below=\"-Dcase=bench_rbyd_id_create -ybench_proged\"" \
-    --subplot-right="-Dcase=bench_rbyd_id_delete -ybench_readed --title=rbyd_delete -W0.2 --xticklabels= \
-        --subplot-below=\"-Dcase=bench_rbyd_id_delete -ybench_proged\""
-
 ./scripts/plotmpl.py bench_rbyd.sh.attr.csv -obench_rbyd.sh.attr.svg \
     -l -xN -bORDER -bCOMMIT --dark \
     -W1600 -H400 --ggplot --y2 --yunits=B \
     --xlabel="attr count" --title="rbyd attr operations" \
     --labels="
-        inorder\,1\,readed,
-        inorder\,1\,proged,
-        inorder\,n\,readed,
-        inorder\,n\,proged,
-        reversed\,1\,readed,
-        reversed\,1\,proged,
-        reversed\,n\,readed,
-        reversed\,n\,proged,
-        random\,1\,readed,
-        random\,1\,proged,
-        random\,n\,readed,
-        random\,n\,proged" \
+        inorder\,readed,
+        inorder\,proged,
+        reversed\,readed,
+        reversed\,proged,
+        random\,readed,
+        random\,proged" \
     --subplot="-Dcase=bench_rbyd_attr_commit -ybench_readed --ylabel=bench_readed --title=rbyd_commit --xticklabels=" \
         --subplot-below="-Dcase=bench_rbyd_attr_commit -ybench_proged --ylabel=bench_proged" \
     --subplot-right="-Dcase=bench_rbyd_attr_fetch -ybench_readed --title=rbyd_fetch -W0.5 --xticklabels= \
@@ -83,10 +53,32 @@ echo "benching ids $id_count,$id_step attrs $attr_count,$attr_step"
     --subplot-right="-Dcase=bench_rbyd_attr_remove -ybench_readed --title=rbyd_remove -W0.2 --xticklabels= \
         --subplot-below=\"-Dcase=bench_rbyd_attr_remove -ybench_proged\""
 
+./scripts/plotmpl.py bench_rbyd.sh.id.csv -obench_rbyd.sh.id.svg \
+    -l -xN -bORDER -bCOMMIT --dark \
+    -W1600 -H400 --ggplot --y2 --yunits=B \
+    --xlabel="id count" --title="rbyd id operations" \
+    --labels="
+        inorder\,readed,
+        inorder\,proged,
+        reversed\,readed,
+        reversed\,proged,
+        random\,readed,
+        random\,proged" \
+    --subplot="-Dcase=bench_rbyd_id_commit -ybench_readed --ylabel=bench_readed --title=rbyd_commit --xticklabels=" \
+        --subplot-below="-Dcase=bench_rbyd_id_commit -ybench_proged --ylabel=bench_proged" \
+    --subplot-right="-Dcase=bench_rbyd_id_fetch -ybench_readed --title=rbyd_fetch -W0.5 --xticklabels= \
+        --subplot-below=\"-Dcase=bench_rbyd_id_fetch -ybench_proged -Y0,1\"" \
+    --subplot-right="-Dcase=bench_rbyd_id_lookup -ybench_readed --title=rbyd_lookup -W0.33 --xticklabels= \
+        --subplot-below=\"-Dcase=bench_rbyd_id_lookup -ybench_proged -Y0,1\"" \
+    --subplot-right="-Dcase=bench_rbyd_id_create -ybench_readed --title=rbyd_create -W0.25 --xticklabels= \
+        --subplot-below=\"-Dcase=bench_rbyd_id_create -ybench_proged\"" \
+    --subplot-right="-Dcase=bench_rbyd_id_delete -ybench_readed --title=rbyd_delete -W0.2 --xticklabels= \
+        --subplot-below=\"-Dcase=bench_rbyd_id_delete -ybench_proged\""
+
 # a simple webpage for easy viewing
 cat << HERE > bench_rbyd.sh.html
     <body style="background-color:#443333;">
-    <img src="bench_rbyd.sh.id.svg">
-    <p></p>
     <img src="bench_rbyd.sh.attr.svg">
+    <p></p>
+    <img src="bench_rbyd.sh.id.svg">
 HERE
